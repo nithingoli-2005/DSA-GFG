979e4caf4ca87799188d084602d897913986f98d
nithingoli-2005 <nithingoli2005@gmail.com>
2025-07-17 23:04:01 +0530

    Successfull Commit

---
+++ Arrays/Day5/nextPermutation.py
@@ -0,0 +1,16 @@
+def next_permutation(arr):
+    n = len(arr)
+    idx =  -1
+    for i in range(n - 2, -1, -1):
+        if arr[i] < arr[i+1]:
+            idx = i
+            break
+    if idx==-1:
+        arr.reverse()
+        return arr
+    for i in range(n - 1, idx, -1):
+        if arr[i] > arr[idx]:
+            arr[i], arr[idx] = arr[idx], arr[i]
+            break
+    arr[idx+1:] = reversed(arr[idx+1:])
+    return arr
